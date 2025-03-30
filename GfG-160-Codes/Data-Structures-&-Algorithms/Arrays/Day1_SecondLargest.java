5c0341ead5a903c6fd8d1d804dbb3a0c01f34ee4
Tilak Kateghar <kateghartilak21@gmail.com>
2025-03-30 15:13:13 +0530

    Update Day1_SecondLargest.java

--- GfG-160-Codes/Data-Structures-&-Algorithms/Arrays/Day1_SecondLargest.java
+++ GfG-160-Codes/Data-Structures-&-Algorithms/Arrays/Day1_SecondLargest.java
@@ -1,1 +1,38 @@
+/* # Second Largest
 
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 and the second largest element does not exist.
+
+Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+*/
+
+// Code-
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]!=arr[n-1]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
